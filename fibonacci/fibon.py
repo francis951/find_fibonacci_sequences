6adfc27b2c27dd071fa55094395411027bc7207b
Francis Murungi <francis_951@live.com>
2023-06-28 17:41:17 +0300

    second failed test

--- fibonacci/fibon.py
+++ fibonacci/fibon.py
@@ -1,8 +1,8 @@
 def fibonacci(num):
     if num <= 0:
         return []
-    # elif num == 1:
-    #     return [0]
+    elif num == 1:
+        return [0]
     # elif num == 2:
     #     return [0, 1]
     # elif num == 3:
--- fibonacci/test_fibon.py
+++ fibonacci/test_fibon.py
@@ -2,5 +2,5 @@ from fibon import fibonacci
 
 def test_fibonacci():
     assert fibonacci(0) == []
-    
+    assert fibonacci(1) == [1]
     
