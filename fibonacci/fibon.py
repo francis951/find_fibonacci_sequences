ce9c61e3f968fb5b008cec644d96d716544c91cd
Francis Murungi <francis_951@live.com>
2023-06-28 17:44:50 +0300

    a third test

--- fibonacci/fibon.py
+++ fibonacci/fibon.py
@@ -3,10 +3,10 @@ def fibonacci(num):
         return []
     elif num == 1:
         return [0]
-    # elif num == 2:
-    #     return [0, 1]
-    # elif num == 3:
-    #     return [0, 1, 2]
+    elif num == 2:
+        return [0, 1]
+    elif num == 3:
+        return [0, 1, 2]
     else:
         fibonacci_sequence = fibonacci(num - 1)
         fibonacci_sequence.append([-1] + fibonacci_sequence[-2])
--- fibonacci/test_fibon.py
+++ fibonacci/test_fibon.py
@@ -3,4 +3,5 @@ from fibon import fibonacci
 def test_fibonacci():
     assert fibonacci(0) == []
     assert fibonacci(1) == [0]
+    assert fibonacci(2) == [0, 1]
     
